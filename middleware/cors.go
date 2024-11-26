c8e47ab9548e8784a038ed1f8b9bc2c2fe90368d
Sanjar Ibrokhimov <sanjaribrokhimov3210@mail.ru>
2024-11-26 17:16:03 +0500

    upd

--- middleware/cors.go
+++ middleware/cors.go
@@ -11,7 +11,20 @@ func CORSMiddleware() gin.HandlerFunc {
 		log.Printf("Incoming request from origin: %s", origin)
 		log.Printf("Request method: %s", c.Request.Method)
 		
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		allowedOrigins := []string{
+			"http://144.126.128.67",
+			"http://localhost:8000",
+			"http://localhost:3000",
+			"*",
+		}
+
+		for _, allowedOrigin := range allowedOrigins {
+			if origin == allowedOrigin || allowedOrigin == "*" {
+				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+				break
+			}
+		}
+		
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
